52c732a14bc05f5c82f18d95a6f0a8be6fd4d151
Nick Johnson <nlj222@gmail.com>
2023-03-07 14:54:24 -0800

    recipe table now generated after selecting ingredients

--- lib/helpers.py
+++ lib/helpers.py
@@ -17,20 +17,25 @@ def create_ingredients_table(ingredients):
     print('-' * 50)
 
 
-def create_recipe_table(recipes):
+def create_recipe_table(session, recipes):
     print('-' * 50)
     print(f'|ID  |RECIPE{" " * 39}|')
     print('-' * 50)
-    recipes_to_display = []
-    for each in selected_ingredients_list:
-        if each in RecipeIngredient.ingredient_id:
-            recipes_to_display.append(each)
+    # recipes_to_display = []
+    # for each in selected_ingredients_list:
+    #     if each in RecipeIngredient.ingredient_id:
+    #         recipes_to_display.append(each)
+
+    recipe_objects = session.query(RecipeIngredient).filter(RecipeIngredient.ingredient_id.in_(selected_ingredients_list)).all()
+    recipe_ids = []
+    for obj in recipe_objects:
+        recipe_ids.append(obj.recipe_id)
 
     for recipe in recipes:
-        
         id_spaces = 4 - len(str(recipe.id))
         name_spaces = 43 - len(recipe.name)
-        if recipe.id in recipes_to_display:
+        # if recipe.id in recipes_to_display:
+        if recipe.id in recipe_ids:
             print(f'|{recipe.id}{" " * id_spaces}|{recipe.name}{" " * name_spaces}|')
     print('-' * 50)
 
@@ -47,9 +52,8 @@ def add_ingredient(session):
                 ingredient_item_id = input('Please enter the ID of your next ingredient: ')
             elif yes_no.lower() in NO:
                 ingredient_item_id = None
-                print('We are friggin done')
-                # recipes = session.query(Recipe)
-                # create_recipe_table(recipes)
+                recipes = session.query(Recipe)
+                create_recipe_table(session, recipes)
 
 
 
