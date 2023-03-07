a7a91e2103df6dfaddd1ad423b4585cfa2b16e8b
triciahughes <hughes.a.tricia@gmail.com>
2023-03-07 11:59:01 -0800

    some cli

--- lib/cli.py
+++ lib/cli.py
@@ -1,7 +1,7 @@
 from db.models import Ingredient
 from db.models import RecipeIngredient
 from db.models import Recipe
-from helpers import (create_ingredients_table, create_recipe_table)
+from helpers import (create_ingredients_table, create_recipe_table, selected_ingredients)
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -15,18 +15,31 @@ session = Session()
 
 if __name__ == '__main__':
     
-    
+    ingredients = session.query(Ingredient)
+    create_ingredients_table(ingredients)
 
-    ingredient_prompt = input('Would you like to select an ingredient? (Y/N): ')
+    ingredient_prompt = input('Line 21: Would you like to select an ingredient? (Y/N): ')
 
-    if ingredient_prompt == 'N':
-        recipes = session.query(Recipe)
-        create_recipe_table(recipes)
-    elif ingredient_prompt == 'Y':
-        ingredients = session.query(Ingredient)
-        create_ingredients_table(ingredients)
-    else:
-        print("Please type Y or N")
+    # if ingredient_prompt == 'N':
+    #     recipes = session.query(Recipe)
+    #     create_recipe_table(recipes)
+    # elif ingredient_prompt == 'Y':
+    #     ingredients = session.query(Ingredient)
+    #     create_ingredients_table(ingredients)
+    # else:
+    #     print("Please type Y or N")
+
+    while ingredient_prompt == 'Y':
+        select_ingredient_prompt = input('Line 33: Select an ingredient by ID: ')
+        selected_ingredients(select_ingredient_prompt)
+
+        if ingredient_prompt == 'N':
+            recipes = session.query(Recipe)
+            create_recipe_table(recipes)
+        else:
+            print("Please type Y or N")
+
+
 
 
 
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,3 +1,7 @@
+YES = ['y', 'ye', 'yes']
+NO = ['n', 'no']
+selected_ingredients_list = []
+
 def create_ingredients_table(ingredients):
     print('-' * 50)
     print(f'|ID  |NAME{" " * 39}|')
@@ -8,12 +12,28 @@ def create_ingredients_table(ingredients):
         print(f'|{ingredient.id}{" " * id_spaces}|{ingredient.name}{" " * name_spaces}|')
     print('-' * 50)
 
+    select_ingredient_prompt = input('Select an ingredient by ID: ')
+
+    selected_ingredients(select_ingredient_prompt)
+    print(selected_ingredients_list)
+
 def create_recipe_table(recipes):
     print('-' * 50)
     print(f'|ID  |RECIPE{" " * 39}|')
     print('-' * 50)
+    recipes_to_display = []
+    for each in selected_ingredients_list:
+        if each in RecipeIngredient.ingredient_id:
+            recipes_to_display.append(each)
+
     for recipe in recipes:
+        
         id_spaces = 4 - len(str(recipe.id))
         name_spaces = 43 - len(recipe.name)
-        print(f'|{recipe.id}{" " * id_spaces}|{recipe.name}{" " * name_spaces}|')
+        if recipe.id in recipes_to_display:
+            print(f'|{recipe.id}{" " * id_spaces}|{recipe.name}{" " * name_spaces}|')
     print('-' * 50)
+
+
+def selected_ingredients(ingredient):
+    selected_ingredients_list.append(ingredient)
